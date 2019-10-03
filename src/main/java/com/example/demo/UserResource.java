383cdf8d977cdd16e9f0cfb9a379f5b06a535d83
Nambi Sankaran <snambi@gmail.com>
2019-10-03 16:17:54 -0700

    initial version

---
+++ src/main/java/com/example/demo/UserResource.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/employees")
+public class UserResource {
+
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey";
+    }
+}
